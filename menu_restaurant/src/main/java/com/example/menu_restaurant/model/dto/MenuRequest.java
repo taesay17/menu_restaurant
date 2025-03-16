6aa8b4a735db3bcb024de920b8d4b727eb101810
taesay17 <151002316+taesay17@users.noreply.github.com>
2025-03-16 23:43:18 +0600

    add unit tests for Hibernate validation

--- menu_restaurant/src/main/java/com/example/menu_restaurant/model/dto/MenuRequest.java
+++ menu_restaurant/src/main/java/com/example/menu_restaurant/model/dto/MenuRequest.java
@@ -1,12 +1,22 @@
 package com.example.menu_restaurant.model.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class MenuRequest {
 
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @Min(value = 1, message = "Price must be greater than 0")
     private double price;
+
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating cannot be more than 5")
     private int rating;
+
+    @NotBlank(message = "Category cannot be blank")
     private String category;
 }
